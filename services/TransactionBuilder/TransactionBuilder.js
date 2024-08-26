464391e007d5a0ce217d201d9bc7ebbf950fd26d
Fabian Scherer <fabiansche@gmail.com>
2024-08-26 22:52:00 +0200

    chore: buy,transfer,roles,vesting (tx builder)

--- services/TransactionBuilder/TransactionBuilder.js
+++ services/TransactionBuilder/TransactionBuilder.js
@@ -1,41 +1,67 @@
+import { encodeSingle, TransactionType } from 'ethers-multisend';
 import {
-  decodeMulti,
-  encodeMulti,
-  encodeSingle,
-  TransactionType,
-} from 'ethers-multisend';
-import {
-  counterAbi,
   bondingCurveAbi,
   paymentRouterAbi,
+  erc20Abi,
 } from '../../abis.js';
 
 export class TransactionBuilder {
-  paymentPusherRole = ''; // TODO
+  PAYMENT_PUSHER_ROLE =
+    '0x5041594d454e545f505553484552000000000000000000000000000000000000';
 
   buy(bondingCurveAddress, depositAmount, minAmountOut) {
-    const tx = encodeSingle({
-      type: TransactionType.callContract,
-      id: '0',
-      to: bondingCurveAddress,
-      value: '0',
-      abi: bondingCurveAbi,
-      functionSignature: 'buy(uint256,uint256)',
-      inputValues: [depositAmount, minAmountOut],
-    });
-    return tx;
+    return this.getEncodedTx(
+      bondingCurveAddress,
+      bondingCurveAbi,
+      'buy(uint256,uint256)',
+      [depositAmount, minAmountOut]
+    );
   }
 
-  assignVestingAdmin(paymentRouter, recipient) {
-    const tx = encodeSingle({
+  transferTokens(token, to, amount) {
+    return this.getEncodedTx(
+      token,
+      erc20Abi,
+      'transfer(address,uint256)',
+      [to, amount]
+    );
+  }
+
+  createVesting(
+    paymentRouter,
+    recipient,
+    token,
+    amount,
+    start,
+    cliff,
+    end
+  ) {
+    return this.getEncodedTx(
+      paymentRouter,
+      paymentRouterAbi,
+      'pushPayment(address,address,uint256,uint256,uint256,uint256)',
+      [recipient, token, amount, start, cliff, end]
+    );
+  }
+
+  assignVestingAdmin(paymentRouter, newRoleOwner) {
+    return this.getEncodedTx(
+      paymentRouter,
+      paymentRouterAbi,
+      'grantModuleRole(bytes32,address)',
+      [this.PAYMENT_PUSHER_ROLE, newRoleOwner]
+    );
+  }
+
+  getEncodedTx(to, abi, functionSignature, inputValues) {
+    return encodeSingle({
       type: TransactionType.callContract,
       id: '0',
-      to: paymentRouter,
+      to,
       value: '0',
-      abi: paymentRouterAbi,
-      functionSignature: 'grantModuleRole',
-      inputValues: [this.paymentPusherRole, recipient],
+      abi,
+      functionSignature,
+      inputValues,
     });
-    return tx;
   }
 }
--- services/TransactionBuilder/TransactionBuilder.test.js
+++ services/TransactionBuilder/TransactionBuilder.test.js
@@ -11,7 +11,7 @@ describe('TransactionBuilder', () => {
       bondingCurveAddress
     );
 
-    it('returns the buy tx', async () => {
+    it('returns the raw tx', async () => {
       const tx = transactionBuilder.buy(
         bondingCurveAddress,
         10n,
@@ -26,5 +26,74 @@ describe('TransactionBuilder', () => {
     });
   });
 
-  describe('TODO: vesting', () => {});
+  describe('#assignVestingAdmin', () => {
+    const paymentRouterAddress =
+      '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'; // mock
+    const recipient = '0x0000000000000000000000000000000000000001';
+
+    it('returns the raw tx', async () => {
+      const transactionBuilder = new TransactionBuilder();
+      const tx = transactionBuilder.assignVestingAdmin(
+        paymentRouterAddress,
+        recipient
+      );
+
+      assert.deepStrictEqual(tx, {
+        to: paymentRouterAddress,
+        value: '0x00',
+        data: '0x6c67e8075041594d454e545f5055534845520000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001',
+      });
+    });
+  });
+
+  describe('#createVesting', () => {
+    const paymentRouterAddress =
+      '0x0000000000000000000000000000000000000001'; // mock
+    const recipient = '0x0000000000000000000000000000000000000002';
+    const token = '0x0000000000000000000000000000000000000003';
+    const amount = 10n;
+    const start = 10n;
+    const cliff = 10n;
+    const end = 10n;
+
+    it('returns the raw tx', async () => {
+      const transactionBuilder = new TransactionBuilder();
+      const tx = transactionBuilder.createVesting(
+        paymentRouterAddress,
+        recipient,
+        token,
+        amount,
+        start,
+        cliff,
+        end
+      );
+
+      assert.deepStrictEqual(tx, {
+        to: paymentRouterAddress,
+        value: '0x00',
+        data: '0x8028b82f00000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000003000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000a',
+      });
+    });
+  });
+
+  describe('#transferTokens', () => {
+    const tokenAddress = '0x0000000000000000000000000000000000000001'; // mock
+    const recipient = '0x0000000000000000000000000000000000000002';
+    const amount = 10n;
+
+    it('returns the raw tx', async () => {
+      const transactionBuilder = new TransactionBuilder();
+      const tx = transactionBuilder.transferTokens(
+        tokenAddress,
+        recipient,
+        amount
+      );
+
+      assert.deepStrictEqual(tx, {
+        to: tokenAddress,
+        value: '0x00',
+        data: '0xa9059cbb0000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000a',
+      });
+    });
+  });
 });
