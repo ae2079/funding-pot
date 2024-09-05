6054b10e37c8417b81f1c94e06b2f24fd8312f92
Fabian Scherer <fabiansche@gmail.com>
2024-09-05 22:21:12 +0200

    test: `execution-flow/getConfigs`

---
+++ execution-flow/getConfigs.test.js
@@ -0,0 +1,26 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import { getConfigs } from './getConfigs.js';
+
+describe('#getConfigs', () => {
+  describe('with existing batch number', () => {
+    const batchNumber = 1;
+    it('returns the config', () => {
+      const batchConfig = getConfigs(batchNumber);
+      assert.deepEqual(Object.keys(batchConfig), [
+        'projectsConfig',
+        'batchConfig',
+      ]);
+    });
+  });
+
+  describe('with non-existing batch number', () => {
+    const batchNumber = 2;
+
+    it('throws an error', () => {
+      assert.throws(() => {
+        getConfigs(batchNumber);
+      });
+    });
+  });
+});
