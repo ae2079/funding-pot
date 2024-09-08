2a1c8192e07548ba2f4790b557d799f7eb8d0b9b
Fabian Scherer <fabiansche@gmail.com>
2024-09-08 23:59:59 +0200

    chore: cleanup config

--- config.js
+++ config.js
@@ -3,11 +3,3 @@ export const CAP = 0.02;
 
 // TRANSACTION BATCH SIZE FOR SAFE
 export const BATCH_SIZE = 100;
-
-// CHAIN_IDS (PROD VS TEST)
-export const PRODUCTION_CHAIN_ID = 420;
-export const TEST_CHAIN_ID = 84532;
-
-// API SPECIFIC
-export const PRODUCTION_ANKR_NETWORK_ID = 'polygon_zkevm';
-export const TEST_ANKR_NETWORK_ID = 'base_sepolia';
